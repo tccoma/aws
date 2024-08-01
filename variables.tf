58fcaebc8a5579514291f95ab50167073297698f
tccome <tccoma@gmail.com>
2024-08-01 11:17:07 +1000

    ec2

--- variables.tf
+++ variables.tf
@@ -1,5 +1,22 @@
 variable "aws_region" {
   type    = string
-  default = "us-east-2"
+  default = "us-west-2"
 }
 
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = "us-west-2a"
+
+  tags = {
+    Name = "Default subnet for us-west-2a"
+  }
+}
+
+variable "db_read_capacity" {
+  type    = number
+  default = 5
+}
+
+variable "db_write_capacity" {
+  type    = number
+  default = 5
+}
