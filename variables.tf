76f056b616998fae71b37cc5c82aa0edf2cfc008
tccome <tccoma@gmail.com>
2024-08-01 11:42:59 +1000

    ec2new

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 variable "aws_region" {
   type    = string
-  default = "us-west-2"
+  default = "us-east-2"
 }
 
 resource "aws_default_subnet" "default_az1" {
-  availability_zone = "us-west-2a"
+  availability_zone = "us-east-2a"
 
   tags = {
     Name = "Default subnet for us-west-2a"
